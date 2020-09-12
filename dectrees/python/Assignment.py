c3ed4b37b549ebec1e2f20e083a866f4adfb6844
basemprince <bshaker@kth.se>
2020-09-13 00:04:54 +0200

    3d commit

--- dectrees/python/Assignment.py
+++ dectrees/python/Assignment.py
@@ -5,7 +5,6 @@ import drawtree_qt5 as draw
 from tabulate import tabulate
 
 def assignment1():
-    draw.drawTree(m.monk2)
     monk1 = ['MONK1']
     monk1.append(round(tree.entropy(m.monk1),5))
     monk2 = ['MONK2']
@@ -13,8 +12,7 @@ def assignment1():
     monk3 = ['MONK3']
     monk3.append(round(tree.entropy(m.monk3),5))
     print(tabulate([monk1, monk2,monk3], headers=['Dataset', 'Entropy'], tablefmt='orgtbl'),'\n')
-
-    
+ 
 def assignment3():
     monk1 = ['monk1']
     monk2 = ['monk2']
@@ -25,15 +23,20 @@ def assignment3():
         monk3.append(round(tree.averageGain(m.monk3,m.attributes[j]),5))
     print(tabulate([monk1, monk2,monk3], headers=['Dataset', 'a1','a2','a3','a4','a5','a6'], tablefmt='orgtbl'))
 
-assignment1()
-assignment3()
-
-t1= tree.buildTree(m.monk1, m.attributes)
-t2= tree.buildTree(m.monk2, m.attributes)
-t3= tree.buildTree(m.monk3, m.attributes)
-
+def assignment5():
+    t1= tree.buildTree(m.monk1, m.attributes)
+    t2= tree.buildTree(m.monk2, m.attributes)
+    t3= tree.buildTree(m.monk3, m.attributes)
+    monk1 = ['MONK1',1-tree.check(t1, m.monk1),round(1-tree.check(t1, m.monk1test),4)]
+    monk2 = ['MONK2',1-tree.check(t2, m.monk2),round(1-tree.check(t2, m.monk2test),4)]
+    monk3 = ['MONK3',1-tree.check(t3, m.monk3),round(1-tree.check(t3, m.monk3test),4)]
+    print(tabulate([monk1, monk2,monk3], headers=['Dataset', 'Error-train','Error-test'], tablefmt='orgtbl'),'\n')
+    draw.drawTree(t1)
+    draw.drawTree(t2)
+    draw.drawTree(t3)
 #print(tree.select(m.monk1,m.attributes[1],2))
 # print(tree.mostCommon(m.monk1))
-# print(tree.check(t1, m.monk1test))
-# print(tree.check(t2, m.monk2test))
-# print(tree.check(t3, m.monk3test))
+
+#assignment1()
+#assignment3()
+assignment5()
